9b590eda66e6e8069447ee6bb968c6096aa22dcc
ryuphi <mcorona.avalos02@gmail.com>
2020-04-22 00:14:25 -0400

    Init commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => (
+  <div>
+    <h1>Y que pasa</h1>
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
