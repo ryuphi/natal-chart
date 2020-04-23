85b73173646d0d7e96782564ab144929c7735786
Marcos Corona <mcorona.avalos02@gmail.com>
2020-04-22 20:14:17 -0400

    Add Horizon axis

--- src/components/Zodiac/ZodiacWheel.js
+++ src/components/Zodiac/ZodiacWheel.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import ZodiacArc from "./ZodiacArc"
+import Horizon from "../Axes/Horizon";
 
 const ZodiacWheel = ({size}) => {
-  const radius = (size / 2) - 10
-  const ascendant = (273 - 273%30)/30%11;
+  const radius = (size / 2) - 50
+  const ascendant = 112
+  const ascendantDegrees = ascendant%30
+  const ascendantSign = (ascendant - ascendantDegrees)/30%11;
 
   const signs = [...Array(12).keys()].map(
-    value => <ZodiacArc sign={(ascendant+value)%12} radius={radius} start={30 * value} end={30 * (value + 1)} correction={3}/>
+    value => <ZodiacArc sign={(ascendantSign+value)%12} radius={radius} start={30 * value} end={30 * (value + 1)} correction={ascendantDegrees}/>
   )
 
   return (
-    <svg width={size} height={size}>
-      <g id='arcs-group' style={{
-        transform: "translate(50%, 50%)"
-      }}>
-        {signs}
-      </g>
-    </svg>
+    <div>
+      <svg width={size} height={size}>
+        <g id='arcs-group' style={{
+          transform: "translate(50%, 50%)"
+        }}>
+          {signs}
+        </g>
+        <g id='axes-group' style={{
+          transform: "translate(50%, 50%)"
+        }}>
+          <Horizon radius={radius}/>
+        </g>
+      </svg>
+    </div>
   )
 }
 
